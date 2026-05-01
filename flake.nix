db199e7b146e5712dd3d98b9db34eb99c262ddff
Pedro Castro <falecompedroac@gmail.com>
2026-05-01 16:49:49 +0100

    added nix flake

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "A Nix flake for the development Next JS web applications.";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    nixpkgs,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import nixpkgs {inherit system;};
+      in {
+        formatter = pkgs.alejandra;
+
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            nodejs_22
+          ];
+        };
+      }
+    );
+}
