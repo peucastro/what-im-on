ae4cc444eff0f5b943bf5b253f2a00b2f4f8146d
Pedro Castro <falecompedroac@gmail.com>
2026-05-02 11:49:59 +0100

    formatting

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -10,10 +10,11 @@ export default function Navbar() {
   const getLinkStyle = (path: string) => {
     const isActive = pathname === path;
 
-    return `flex-1 rounded-xl border-2 py-3 text-center transition-all ${isActive
-      ? 'bg-zinc-100 text-black font-bold border-zinc-300'
-      : 'bg-transparent text-black border-zinc-300 hover:text-zinc-800'
-      }`;
+    return `flex-1 rounded-xl border-2 py-3 text-center transition-all ${
+      isActive
+        ? 'bg-zinc-100 text-black font-bold border-zinc-300'
+        : 'bg-transparent text-black border-zinc-300 hover:text-zinc-800'
+    }`;
   };
 
   return (
