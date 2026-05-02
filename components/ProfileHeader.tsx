cd7bf26f82a0cf60c46d7ce64cd9221bdf6de691
Pedro Castro <falecompedroac@gmail.com>
2026-05-02 15:27:12 +0100

    improved header

--- components/ProfileHeader.tsx
+++ components/ProfileHeader.tsx
@@ -3,5 +3,13 @@ interface ProfileHeaderProps {
 }
 
 export default function ProfileHeader({ username }: ProfileHeaderProps) {
-  return <h1 className="text-3xl font-bold text-black lowercase">what {username}&apos;s on</h1>;
+  return (
+    <h1 className="text-3xl font-normal text-black lowercase">
+      what
+      <br />
+      <span className="font-semibold">{username}&apos;s</span>
+      <br />
+      on
+    </h1>
+  );
 }
